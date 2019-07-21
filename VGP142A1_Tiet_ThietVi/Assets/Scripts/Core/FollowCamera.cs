7686898be0f2c7c738de26adf3372593c5bececd
Vi Tiet <tuxedo619_2010@yahoo.com>
2019-07-21 19:47:05 -0400

    Add Unity Project
    Cross-platform Programming Assignment 1

---
+++ VGP142A1_Tiet_ThietVi/Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!target)
+        {
+            target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        }
+
+        transform.position = target.position;
+    }
+}

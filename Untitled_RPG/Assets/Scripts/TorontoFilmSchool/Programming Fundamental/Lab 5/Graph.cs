2c0016e059ac81b6bc9064de14de0af817fea65b
Vi Tiet <vitiet.programmer@gmail.com>
2020-12-13 20:26:58 -0500

    Project Update

--- Untitled_RPG/Assets/Scripts/TorontoFilmSchool/Programming Fundamental/Lab 5/Graph.cs
+++ Untitled_RPG/Assets/Scripts/TorontoFilmSchool/Programming Fundamental/Lab 5/Graph.cs
@@ -139,22 +139,3 @@ public class Graph<T>
         }
     }
 }
-
-public static class IListExtensions
-{
-    /// <summary>
-    /// Shuffles the element order of the specified list.
-    /// </summary>
-    public static void Shuffle<T>(this IList<T> ts)
-    {
-        int count = ts.Count;
-        int last = count - 1;
-        for (int i = 0; i < last; i++)
-        {
-            int r = Random.Range(i, count);
-            var temp = ts[i];
-            ts[i] = ts[r];
-            ts[r] = temp;
-        }
-    }
-}
